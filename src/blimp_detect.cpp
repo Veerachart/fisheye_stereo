baf93908b43e901f5fb2658781eeee66b669c7bc
Veerachart Srisamosorn <veerachart.sr@gmail.com>
2016-08-30 15:32:27 +0900

    Added morphology to remove noise and find contours. The contours are sorted in order of the area. Parameters need to be tuned.

--- src/blimp_detect.cpp
+++ src/blimp_detect.cpp
@@ -15,12 +15,19 @@
 #include "geometry_msgs/Point.h"
 #include "geometry_msgs/PointStamped.h"
 #include <Eigen/Core>
+#include <algorithm>
 
 using namespace cv;
 using namespace Eigen;
 namespace enc = sensor_msgs::image_encodings;
 
 const double PI = 3.141592653589793;
+
+bool compareContourAreas ( vector<Point> contour1, vector<Point> contour2 ) {
+    double i = contourArea(Mat(contour1));
+    double j = contourArea(Mat(contour2));
+    return ( i > j );
+}
 
 class BlimpTracker {
     ros::NodeHandle nh_;
@@ -41,6 +48,11 @@ class BlimpTracker {
     Mat img_gray;
     Mat temp;
     
+    Mat show;
+    Mat bg_show;
+    Mat contour_show;
+    float scale;
+    
     public:
         BlimpTracker()
             : it_(nh_){
@@ -52,9 +64,11 @@ class BlimpTracker {
             is_bg_built = FALSE;
             bg_count = 0;
             bg_limit = 25;      // 1 s
+            scale = 0.5;
         }
         
         void imageCallback (const sensor_msgs::Image::ConstPtr& msg) {
+            ros::Time begin = ros::Time::now();
             cv_bridge::CvImagePtr cv_ptr;
             try
             {
@@ -83,7 +97,30 @@ class BlimpTracker {
                 return;
             }
             absdiff(background, img_gray, foreground);
-            imshow("Foreground", foreground);
+            threshold(foreground, foreground, 25, 255, THRESH_BINARY);
+            morphologyEx(foreground, foreground, MORPH_OPEN, Mat::ones(3,3,CV_8U), Point(-1,-1), 1);
+            morphologyEx(foreground, foreground, MORPH_CLOSE, Mat::ones(3,3,CV_8U), Point(-1,-1), 3);
+            morphologyEx(foreground, foreground, MORPH_OPEN, Mat::ones(3,3,CV_8U), Point(-1,-1), 3);
+            
+            vector<vector<Point> > contours;
+            findContours(foreground, contours, CV_RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+            if(contours.size() > 1){
+                std::sort(contours.begin(), contours.end(), compareContourAreas);
+                //drawContours(cv_ptr->image, contours, 0, Scalar(128,255,255), 3, CV_AA);
+                //drawContours(cv_ptr->image, contours, 1, Scalar(255,0,0), 3, CV_AA);
+            }
+            else if (contours.size()){
+                // Only one
+                //drawContours(cv_ptr->image, contours, -1, Scalar(128,255,255), 3, CV_AA);
+            }
+            //resize(foreground, show, Size(), scale, scale);
+            //resize(background, bg_show, Size(), scale, scale);
+            //resize(cv_ptr->image, contour_show, Size(), scale, scale);
+            //imshow("Background", bg_show);
+            //imshow("Foreground", show);
+            //imshow("contours", contour_show);
+            double loop_time = (ros::Time::now() - begin).toSec();
+            ROS_INFO("%.6f", loop_time);
             waitKey(1);
         }
                 
